2983d917aac5a54a1301d5f5f64084a1e257fc81
Ruchir Mandora <ruchirm702@gmail.com>
2024-09-10 22:46:57 +0530

    Initial commit with base project setup

---
+++ src/main/java/dev/ruchir/evolvion_hr_service/EvolvionHrServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.ruchir.evolvion_hr_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvolvionHrServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvolvionHrServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/ruchir/evolvion_hr_service/EvolvionHrServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ruchir.evolvion_hr_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvolvionHrServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
