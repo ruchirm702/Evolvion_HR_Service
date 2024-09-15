b2d3174885b2f975e9e3755c5d7826f500f2152c
Ruchir Mandora <ruchirm702@gmail.com>
2024-09-15 11:16:30 +0530

    Implemented Payroll Repository

---
+++ src/main/java/dev/ruchir/evolvion_hr_service/repository/PayrollRepository.java
@@ -0,0 +1,15 @@
+package dev.ruchir.evolvion_hr_service.repository;
+
+import dev.ruchir.evolvion_hr_service.model.core.Payroll;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.Optional;
+import java.util.List;
+
+public interface PayrollRepository extends JpaRepository<Payroll, Long> {
+
+    Optional<Payroll> findByEmployeeIdAndPaymentDate(Long employeeId, LocalDate paymentDate);
+    List<Payroll> findAllByEmployeeId(Long employeeId);
+    List<Payroll> findAllByStatus(String status);
+}
