71ea88e4b523478a1adf795bcdaf6d5dea7753bb
Ruchir Mandora <ruchirm702@gmail.com>
2024-09-17 01:42:37 +0530

    Added Custome Exceptions

---
+++ src/main/java/dev/ruchir/evolvion_hr_service/controller_advise/AttendanceNotFoundException.java
@@ -0,0 +1,8 @@
+package dev.ruchir.evolvion_hr_service.controller_advise;
+
+public class AttendanceNotFoundException extends RuntimeException {
+
+    public AttendanceNotFoundException(String message) {
+        super(message);
+    }
+}
